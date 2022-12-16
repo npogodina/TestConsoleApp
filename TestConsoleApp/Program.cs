628c610a25f970c220026328f28100edb0cec3bb
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-16 10:31:45 -0800

    SOLID: Open Closed Principle

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,47 +1,130 @@
-﻿using System.Diagnostics;
+﻿namespace DesignPatterns;
 
-namespace DesignPatterns;
+// SOLID: Open Closed Principle
+// Open to extension and closed to modification.
+// We should not modify existing classes once shipped.
+// We could ship new modules adding new functionality via inheritance through interfaces.
 
-// SOLID: Single Responsibility Principle
-
-/// <summary>
-/// Dedicated class to create journal and journal entries
-/// </summary>
-public class Journal
+public enum Color
 {
-    private readonly List<string> entries = new List<string>();
+    Red, Blue, Green
+}
 
-    private static int count = 0;
+public enum Size
+{
+    Small, Medium, Large
+}
 
-    public int AddEntry(string text)
+public class Product
+{
+    public string Name;
+    public Color Color;
+    public Size Size;
+
+    public Product(string name, Color color, Size size)
     {
-        count++;
-        entries.Add($"{count}: {text}");
-        return count; // memento
-    }
+        if (name == null)
+        {
+            throw new ArgumentNullException(paramName: nameof(name));
+        }
 
-    public void RemoveEntry(int index)
-    {
-        entries.RemoveAt(index);
-    }
-
-    public override string ToString()
-    {
-        return string.Join(Environment.NewLine, entries);
+        Name = name;
+        Color = color;
+        Size = size;
     }
 }
 
-/// <summary>
-/// Dedicated class to save journal entries
-/// </summary>
-public class Persistance
+public class ProductFilter
 {
-    public void SaveToFile(Journal journal, string filename, bool overwrite = false) 
+    public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
     {
-        if (overwrite || !File.Exists(filename)) 
+        foreach (var p in products) 
         {
-            var content = journal.ToString();
-            File.WriteAllText(filename, content);
+        if (p.Size == size)
+            {
+                yield return p;
+            }
+        }
+    }
+
+    public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+    {
+        foreach (var p in products)
+        {
+            if (p.Color == color)
+            {
+                yield return p;
+            }
+        }
+    }
+}
+
+public interface ISpecification<T>
+{
+    bool IsSatisfied(T t);
+}
+
+public class ColorSpecification : ISpecification<Product>
+{
+    private Color color;
+
+    public ColorSpecification(Color color)
+    {
+        this.color = color;
+    }
+
+    public bool IsSatisfied(Product product)
+    {
+        return product.Color == color;
+    }
+}
+
+public class SizeSpecification : ISpecification<Product>
+{
+    private Size size;
+
+    public SizeSpecification(Size size)
+    {
+        this.size = size;
+    }
+
+    public bool IsSatisfied(Product product)
+    {
+        return product.Size == size;
+    }
+}
+
+public class AndSpecification<T> : ISpecification<T> // Combinator
+{
+    private ISpecification<T> first, second;
+
+    public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+    {
+        this.first = first ?? throw new ArgumentNullException(nameof(first));
+        this.second = second ?? throw new ArgumentNullException(nameof(second));
+    }
+
+    public bool IsSatisfied(T t)
+    {
+        return first.IsSatisfied(t) && second.IsSatisfied(t);
+    }
+}
+
+public interface IFilter<T>
+{
+    IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specification);
+}
+
+public class BetterFilter : IFilter<Product>
+{
+    public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> specification)
+    {
+        foreach (var item in items)
+        {
+            if (specification.IsSatisfied(item))
+            {
+                yield return item;
+            }
         }
     }
 }
@@ -50,22 +133,44 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Journal journal = new Journal();
-        journal.AddEntry("My first entry");
-        journal.AddEntry("My second entry");
-        journal.AddEntry("My third entry");
-        //journal.RemoveEntry(1);
-        Console.WriteLine(journal);
+        var apple = new Product("Apple", Color.Green, Size.Small);
+        var tree = new Product("Tree", Color.Green, Size.Large);
+        var house = new Product("House", Color.Blue, Size.Large);
 
-        Persistance persistance = new Persistance();
-        var filename = @"C:\Temp\TestJournal.txt";
-        persistance.SaveToFile(journal, filename, true);
+        Product[] products = { apple, tree, house };
+        
+        Console.WriteLine("Green products (old):");
+        var pf = new ProductFilter();
 
-        var p = new Process();
-        p.StartInfo = new ProcessStartInfo(filename)
+        foreach (var p in pf.FilterByColor(products, Color.Green))
         {
-            UseShellExecute = true
-        };
-        p.Start();
+           Console.WriteLine($" - {p.Name} is green");
+        }
+
+        var betterFilter = new BetterFilter();
+
+        Console.WriteLine("Green products (new):");
+
+        var greenProducts = betterFilter.Filter(products, new ColorSpecification(Color.Green));
+
+        foreach (var product in greenProducts)
+        {
+            Console.WriteLine($" - {product.Name} is green");
+        }
+
+
+        Console.WriteLine("Large Blue products (new):");
+
+        var largeBlueProducts = betterFilter.Filter(
+            products, 
+            new AndSpecification<Product>(
+                new ColorSpecification(Color.Blue), 
+                new SizeSpecification(Size.Large)
+            ));
+
+        foreach (var product in largeBlueProducts)
+        {
+            Console.WriteLine($" - {product.Name} is large and blue");
+        }
     }
 }
