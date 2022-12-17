920c7b2f10fff51aa634d4004b7c9cf0a4b70b74
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-16 17:43:34 -0800

    SOLID: Interface Segregation

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,65 +1,53 @@
 ﻿namespace DesignPatterns;
 
-// SOLID: Liscov Substitution Principle
-// We should be able to substitute a base type for a sub type.
-// You should be able to upcast to your base type.
-
-public class Rectangle
-{
-    public virtual int Width { get; set; }
-    public virtual int Height { get; set; }
-
-    public Rectangle()
-    {
-
-    }
-
-    public Rectangle(int width, int height)
-    {
-        this.Width = width; 
-        this.Height = height;
-    }
-
-    public override string ToString()
-    {
-        return $"{nameof(Width)}: {Width}; {nameof(Height)}: {Height}";
-    }
-}
-
-public class Square : Rectangle
-{
-    public override int Width
-    {
-        set
-        {
-            base.Width = base.Height = value;
-        }
-    }
-
-    public override int Height
-    {
-        set
-        {
-            base.Height = base.Width = value;
-        }
-    }
-}
+// SOLID: Interface Segregation
+// Interfaces shouldn't be too large
+// Don't make consumers implement methods they don't need
+// (don't make them pay for what they don't need)
+// Have multiple small interfaces instead
 
 public class Program
 {
-    public static int Area(Rectangle rectangle) => rectangle.Width * rectangle.Height;
+    public interface IPrinter
+    {
+        void Print();
+    }
+
+    public interface IScanner
+    {
+        void Scan();
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner
+    {
+
+    }
+
+    public class MultiFunctionDevice : IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionDevice(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer ?? throw new ArgumentNullException(paramName: nameof(printer));
+            this.scanner = scanner ?? throw new ArgumentNullException(paramName: nameof(scanner));
+        }
+
+        public void Print()
+        {
+            printer.Print(); // Decorator pattern
+        }
+
+        public void Scan()
+        {
+            scanner.Scan(); // Decorator pattern
+        }
+    }
 
     static void Main(string[] args)
     {
 
-        var rc = new Rectangle(2, 3);
-        Console.WriteLine($"{rc}; {nameof(Area)}: {Area(rc)}");
-
-        Rectangle square = new Square();
-        square.Width = 3;
-        Console.WriteLine($"{square}; {nameof(Area)}: {Area(square)}");
 
     }
-
-    
 }
