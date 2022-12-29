cda669f8a75a8ea35fb93face317d858b903707b
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-29 13:46:19 -0800

    Asynchronous Factory method

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,17 +1,24 @@
 ﻿namespace Coding.Exercise;
 
+// Async Factory method allows to initialize object fully and in asynchronous manner
+
 public class Foo
 {
-    public Foo()
+    private Foo()
     {
 
     }
 
-    // One way to have an async operation in Constructor
-    public async Task<Foo> InitAsync()
+    private async Task<Foo> InitAsync()
     {
         await Task.Delay(1000);
         return this;
+    }
+
+    public static Task<Foo> CreateAsync()
+    {
+        var foo = new Foo();
+        return foo.InitAsync();
     }
 }
 
@@ -19,7 +26,6 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var foo = new Foo();
-        await foo.InitAsync();
+        Foo x = await Foo.CreateAsync();
     }
 }
