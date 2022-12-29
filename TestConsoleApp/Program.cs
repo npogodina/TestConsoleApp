e13876e9b5ab97c52acb2f5a2d3d1e38db3e25d8
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-29 13:20:42 -0800

    Factory method

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,100 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace Coding.Exercise
+﻿namespace Coding.Exercise
 {
-    public enum AccessModifier
+    public class Point
     {
-        Public,
-        Private,
-        Protected
-    }
+        private double x, y;
 
-    public class CodeBuilder
-    {
-        public CodeBlock Root = new CodeBlock();
-        private const string DefaultRootType = "class";
-        public class CodeBlock
+        private Point(double x, double y)
         {
-
-            // You could just make your data members public without using this shortcut
-            // but then if you decided to change the implementation of the data member to have some logic
-            // then you would need to break the interface.
-            // So in short it is a shortcut to create more flexible code.
-            public string Name { get; set; } = string.Empty;
-
-            public string Type { get; set; } = string.Empty;
-
-            public AccessModifier AccessModifier { get; set; } = AccessModifier.Public;
-
-            private const int IndentationStep = 2;
-
-            public CodeBlock()
-            {
-            }
-
-            public CodeBlock(
-                string name,
-                string type,
-                AccessModifier accessModifier = AccessModifier.Public)
-            {
-                this.Name = name;
-                this.Type = type;
-                this.AccessModifier = accessModifier;
-            }
-
-            public List<CodeBlock> Fields = new List<CodeBlock>();
-
-            public override string ToString()
-            {
-                return ToStringImpl();
-            }
-
-            private string ToStringImpl(int indent = 0)
-            {
-                var code = new StringBuilder();
-                var indentation = new string(' ', indent * IndentationStep);
-                code.Append($"{indentation}{AccessModifier.ToString().ToLower()} {Type} {Name}");
-
-                if (Fields.Any())
-                {
-                    code.AppendLine("\n{");
-
-                    foreach (var field in Fields) // continue here!
-                    {
-                        code.AppendLine($"{ field.ToStringImpl(indent + 1)};");
-                    }
-
-                    code.Append("}");
-                }
-                return code.ToString();
-            }
+            this.x = x;
+            this.y = y;
         }
 
-        public CodeBuilder(
-            string name, 
-            string type = DefaultRootType,
-            AccessModifier accessModifier = AccessModifier.Public)
+        // Factory method
+        public static Point NewCartesianPoint(double x, double y)
         {
-            Root = new CodeBlock(name, type, accessModifier);
+            return new Point(x, y);
         }
 
-        public CodeBuilder AddField(
-            string name, 
-            string type, 
-            AccessModifier accessModifier = AccessModifier.Public)
+        // Factory method:
+        // Overload with the same number of arguments!
+        // Unique names!
+        // ( = API improvement)
+        public static Point NewPolarPoint(double rho, double theta)
         {
-            var field = new CodeBlock(name, type, accessModifier);
-            Root.Fields.Add(field);
-            return this;
-        }
-
-        public override string ToString()
-        {
-            return Root.ToString();
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
         }
     }
 
@@ -102,8 +30,7 @@ namespace Coding.Exercise
     {
         static void Main(string[] args)
         {
-            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
-            Console.WriteLine(cb);
+            var point = Point.NewCartesianPoint(4.50, 6.00);
         }
     }
 }
