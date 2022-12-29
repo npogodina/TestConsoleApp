ff21f95fbc3fffc581f0822c23df88643f6c2860
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-29 12:48:35 -0800

    Builder Coding Exercise

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,105 +1,109 @@
-﻿namespace DesignPatterns;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-// Faceted Builder pattern
-
-public class Person
+namespace Coding.Exercise
 {
-    // Address
-    public string StreetAddress, Postcode, City;
-
-    // Employment
-    public string Company, Position;
-    public int Salary;
-}
-
-public class PersonBuilder // Facade
-{
-    // reference!
-    protected Person person = new Person();
-
-    public PersonJobBuilder Works => new PersonJobBuilder(person);
-
-    public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
-
-    //public static implicit operator Person(PersonBuilder pb)
-    //{
-    //    return pb.person;
-    //}
-
-    public Person BuildPerson()
+    public enum AccessModifier
     {
-        return person;
-    }
-}
-
-// can be inner classes of PersonBuilder to hind internals from the consumer
-
-public class PersonJobBuilder : PersonBuilder
-{
-    public PersonJobBuilder(Person person)
-    {
-        this.person = person;
+        Public,
+        Private,
+        Protected
     }
 
-    public PersonJobBuilder At(string company)
+    public class CodeBuilder
     {
-        this.person.Company = company;
-        return this;
+        public CodeBlock Root = new CodeBlock();
+        private const string DefaultRootType = "class";
+        public class CodeBlock
+        {
+
+            // You could just make your data members public without using this shortcut
+            // but then if you decided to change the implementation of the data member to have some logic
+            // then you would need to break the interface.
+            // So in short it is a shortcut to create more flexible code.
+            public string Name { get; set; } = string.Empty;
+
+            public string Type { get; set; } = string.Empty;
+
+            public AccessModifier AccessModifier { get; set; } = AccessModifier.Public;
+
+            private const int IndentationStep = 2;
+
+            public CodeBlock()
+            {
+            }
+
+            public CodeBlock(
+                string name,
+                string type,
+                AccessModifier accessModifier = AccessModifier.Public)
+            {
+                this.Name = name;
+                this.Type = type;
+                this.AccessModifier = accessModifier;
+            }
+
+            public List<CodeBlock> Fields = new List<CodeBlock>();
+
+            public override string ToString()
+            {
+                return ToStringImpl();
+            }
+
+            private string ToStringImpl(int indent = 0)
+            {
+                var code = new StringBuilder();
+                var indentation = new string(' ', indent * IndentationStep);
+                code.Append($"{indentation}{AccessModifier.ToString().ToLower()} {Type} {Name}");
+
+                if (Fields.Any())
+                {
+                    code.AppendLine("\n{");
+
+                    foreach (var field in Fields) // continue here!
+                    {
+                        code.AppendLine($"{ field.ToStringImpl(indent + 1)};");
+                    }
+
+                    code.Append("}");
+                }
+                return code.ToString();
+            }
+        }
+
+        public CodeBuilder(
+            string name, 
+            string type = DefaultRootType,
+            AccessModifier accessModifier = AccessModifier.Public)
+        {
+            Root = new CodeBlock(name, type, accessModifier);
+        }
+
+        public CodeBuilder AddField(
+            string name, 
+            string type, 
+            AccessModifier accessModifier = AccessModifier.Public)
+        {
+            var field = new CodeBlock(name, type, accessModifier);
+            Root.Fields.Add(field);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Root.ToString();
+        }
     }
 
-    public PersonJobBuilder As(string position)
+    public class Program
     {
-        this.person.Position = position;
-        return this;
-    }
-
-    public PersonJobBuilder Making(int salary)
-    {
-        this.person.Salary = salary;
-        return this;
-    }
-}
-
-public class PersonAddressBuilder : PersonBuilder
-{
-    public PersonAddressBuilder(Person person)
-    {
-        this.person = person;
-    }
-
-    public PersonAddressBuilder Street(string street)
-    {
-        this.person.StreetAddress = street;
-        return this;
-    }
-
-    public PersonAddressBuilder Postcode(string postcode)
-    {
-        this.person.Postcode = postcode;
-        return this;
-    }
-
-    public PersonAddressBuilder City(string city)
-    {
-        this.person.City = city;
-        return this;
-    }
-}
-   
-public class Program
-{
-    static void Main(string[] args)
-    {
-        var pb = new PersonBuilder();
-        Person person = pb
-            .Works
-                .At("Microsoft")
-                .As("Software Engineer")
-                .Making(140000)
-            .Lives
-                .Street("33333 333 DR SE")
-                .Postcode("33333")
-                .City("Monroe")
-            .BuildPerson();              
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+        }
     }
 }
