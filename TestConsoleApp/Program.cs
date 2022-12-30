ec54cc403b7f9c11c25aae37583cd255ce60fac6
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-30 10:15:54 -0800

    Object tracking via Factory

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,37 +1,63 @@
-﻿namespace Coding.Exercise;
+﻿using System.Text;
 
-// Single Responcibility Principle:
-// Instantiaing a class is a separate responcibility!
-public static class PointFactory
+namespace ObjectTracking;
+
+public interface ITheme
 {
-    public static Point NewCartesianPoint(double x, double y)
+    string TextColor { get; }
+    string BgrColor { get; }
+}
+
+public class LightTheme : ITheme
+{
+    public string TextColor => "Black";
+    public string BgrColor => "White";
+}
+
+public class DarkTheme : ITheme
+{
+    public string TextColor => "White";
+    public string BgrColor => "Dark Gray";
+}
+
+public class TrackingThemeFactory
+{
+    // About weak references:
+    // https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/weak-references
+    private readonly List<WeakReference<ITheme>> themes = new();
+    public ITheme CreateTheme(bool dark)
     {
-        return new Point(x, y);
+        ITheme theme = dark ? new DarkTheme() : new LightTheme();
+        themes.Add(new WeakReference<ITheme>(theme));
+        return theme;
     }
 
-    public static Point NewPolarPoint(double rho, double theta)
+    public string Info
     {
-        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        get
+        {
+            var sb = new StringBuilder();
+            foreach (var reference in themes)
+            {
+                if (reference.TryGetTarget(out var theme))
+                {
+                    bool dark = theme is DarkTheme;
+                    sb.Append(dark ? "Dark" : "Light");
+                    sb.AppendLine(" Theme");
+                }
+            }
+            return sb.ToString();
+        }
     }
 }
 
-public class Point
+public class Demo
 {
-    private double x, y;
-
-    // Can be called only to create a new cartesian point
-    public Point(double x, double y)
+    public static void Main(string[] args)
     {
-        this.x = x;
-        this.y = y;
+        var factory = new TrackingThemeFactory();
+        var theme1 = factory.CreateTheme(false);
+        var theme2 = factory.CreateTheme(true);
+        Console.WriteLine(factory.Info);
     }
 }
-
-public class Program
-{
-    static void Main(string[] args)
-    {
-        var point = PointFactory.NewCartesianPoint(4.50, 6.00);
-    }
-}
-
