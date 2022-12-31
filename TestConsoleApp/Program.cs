9802cb840b5b55c079ad3c23f4086981964abd3d
Nataliya Pogodina <n.pogodina@gmail.com>
2022-12-31 11:52:34 -0800

    Abstract Factory with Reflection

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace AbstractFactory;
 
-public enum HotDrink
-{
-    Tea,
-    Coffee
-}
+// Honoring Open-Closed Principle by
+// Replacing the inner enum of available drinks with Reflection
+
+// Reflection
+// Reflection provides objects (of type Type) that describe assemblies, modules, and types.
+// You can use reflection to dynamically create an instance of a type,
+// bind the type to an existing object,
+// or get the type from an existing object and invoke its methods or access its fields and properties.
+// (from: https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/reflection)
+
+// Better approach: use Dependency Injection
 
 public interface IHotDrink
 {
@@ -52,31 +58,80 @@ internal class CoffeeFactory : IHotDrinkFactory
 
 public class HotDrinkMachine
 {
-    public enum AvailableDrink
-    {
-        Tea,
-        Coffee
-    }
+    //public enum AvailableDrink
+    //{
+    //    Tea,
+    //    Coffee
+    //}
 
-    private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
-        new Dictionary<AvailableDrink, IHotDrinkFactory>();
+    //private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+    //    new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+    //public HotDrinkMachine()
+    //{
+    //    foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+    //    {
+    //        var factory = (IHotDrinkFactory)Activator.CreateInstance(
+    //            Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+
+    //        factories.Add(drink, factory);
+    //    }
+    //}
+
+    //public IHotDrink MakeDrink(AvailableDrink type, int amount)
+    //{
+    //    var machine = factories[type];
+    //    var drink = machine.Prepare(amount);
+    //    return drink;
+    //}
+
+    private List<Tuple<string, IHotDrinkFactory>> factories = 
+        new List<Tuple<string, IHotDrinkFactory>>();
 
     public HotDrinkMachine()
     {
-        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
         {
-            var factory = (IHotDrinkFactory)Activator.CreateInstance(
-                Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
-
-            factories.Add(drink, factory);
+            if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+            {
+                factories.Add(Tuple.Create(
+                    t.Name.Replace("Factory", string.Empty),
+                    (IHotDrinkFactory)Activator.CreateInstance(t)));
+            }
         }
     }
 
-    public IHotDrink MakeDrink(AvailableDrink type, int amount)
+    public IHotDrink MakeDrink()
     {
-        var machine = factories[type];
-        var drink = machine.Prepare(amount);
-        return drink;
+        Console.WriteLine("Available drinks:");
+        for (int i = 0; i < factories.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {factories[i].Item1}");
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Please, enter drink number.");
+            var userChoice = Console.ReadLine();
+            if (userChoice != null &&
+                int.TryParse(userChoice, out int drinkNumber) &&
+                drinkNumber > 0 &&
+                drinkNumber <= 2)
+            {
+                Console.WriteLine("Please, enter amount in ml.");
+                var userAmount = Console.ReadLine();
+                if (userAmount != null &&
+                    int.TryParse(userAmount, out int amount) &&
+                    amount > 0)
+                {
+                    Console.WriteLine("Thank you!");
+                    var drink = factories[drinkNumber - 1].Item2.Prepare(amount);
+                    return drink;
+                }
+            }
+
+            Console.WriteLine("Incorrect input! Please, try again!");
+        }
     }
 }
 
@@ -85,7 +140,11 @@ public class Demo
     public static void Main(string[] args)
     {
         var machine = new HotDrinkMachine();
-        var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 200);
+        
+        //var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 200);
+        //drink.Consume();
+
+        var drink = machine.MakeDrink();
         drink.Consume();
     }
 }
