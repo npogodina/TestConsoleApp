de9ecde68fa5cebc0890edd72d0430151089fb22
Nataliya Pogodina <n.pogodina@gmail.com>
2023-01-04 16:23:37 -0800

    Singleton pattern

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,71 +1,61 @@
-﻿namespace Coding.Exercise
+﻿using MoreLinq;
+
+namespace Singleton;
+
+// Singleton = controversial design pattern
+// Component that is instatiated only once.
+
+// Motivation:
+// 1) For some components it makes sense to have only one in the system
+// - database repository
+// - object factory
+// 2) Constructor call is expensive. We want to:
+// - do it once
+// - provide everyone with the same instance
+// 3) Want to prevent anyone from creating additinal copies
+// 4) Need to take care of lazy instantiation and threat safety
+
+public interface IDatabase
 {
-    public interface IDeepCopiable<T>
+    int GetPopulation(string city);
+}
+
+public class SingletonDatabase : IDatabase
+{
+    private Dictionary<string, int> capitals;
+
+    private SingletonDatabase()
     {
-        T DeepCopy();
+        Console.WriteLine("Initializing database...");
+        capitals = File.ReadAllLines("Capitals.txt")
+            .Batch(2)
+            .ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1)));
     }
 
+    // Use lazy initialization to defer the creation of a large or resource-intensive object,
+    // or the execution of a resource-intensive task,
+    // particularly when such creation or execution might not occur during the lifetime of the program.
+    // https://learn.microsoft.com/en-us/dotnet/api/system.lazy-1?redirectedfrom=MSDN&view=net-7.0
+    private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
 
-    public class Point : IDeepCopiable<Point>
+    public static SingletonDatabase Instance => instance.Value;
+
+    public int GetPopulation(string city)
     {
-        public int X, Y;
-
-        public Point DeepCopy()
-        {
-            return new Point
-            {
-                X = this.X,
-                Y = this.Y
-            };
-        }
+        return capitals[city];
     }
+}
 
-    public class Line : IDeepCopiable<Line>
+public class Demo
+{
+    static void Main()
     {
-        public Point Start, End;
+        var db = SingletonDatabase.Instance;
 
-        public Line DeepCopy()
-        {
-            return new Line
-            {
-                Start = this.Start.DeepCopy(),
-                End = this.End.DeepCopy()
-            };
-        }
-
-        public override string ToString()
-        {
-            return $"Line: Start = {Start.X}, {Start.Y}; End = {End.X}, {End.Y}";
-        }
-    }
-
-    public class Demo
-    {
-        public static void Main(string[] args)
-        {
-            var line = new Line
-            {
-                Start = new Point
-                {
-                    X = 1,
-                    Y = 2
-                },
-                End = new Point
-                {
-                    X = 3,
-                    Y = 4
-                }
-            };
-
-            Console.WriteLine($"Original. {line.ToString()}");
-
-            var lineCopy = line.DeepCopy();
-            Console.WriteLine($"Copy. {lineCopy.ToString()}");
-
-            lineCopy.Start.X = 9;
-            Console.WriteLine($"Modified Copy. {lineCopy.ToString()}");
-
-            Console.WriteLine($"Original. {line.ToString()}");
-        }
+        // works just fine while you're working with a real database.
+        var city = "Tokyo";
+        Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
     }
 }
