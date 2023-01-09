8161d6f60ee162b5a11c41305155a7302344d0cd
Nataliya Pogodina <n.pogodina@gmail.com>
2023-01-09 11:15:22 -0800

    Adapter pattern

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,26 +1,125 @@
-﻿using System;
+﻿using MoreLinq;
+using System.Collections.ObjectModel;
 
-namespace Coding.Exercise
+namespace Adapter;
+
+// Adapts an interface you got to the interface you have
+
+public class Point
 {
-    public class SingletonTester
+    public int X, Y;
+
+	public Point(int x, int y)
+	{
+		this.X = x;
+		this.Y = y;
+	}
+
+    public override string ToString()
     {
-        /// <summary>
-        /// Determines if an object created by factory method is a singleton or not
-        /// </summary>
-        /// <param name="func">Factory method</param>
-        public static bool IsSingleton(Func<object> func)
-        {
-            // Get two objects and check for referential equality.
-            // Reference equality means that the object variables that are compared refer to the same object. 
-            // https://learn.microsoft.com/en-us/dotnet/api/system.object.equals?view=net-7.0
+        return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+    }
+}
 
-            var obj1 = func();
-            var obj2 = func();
+public class Line
+{
+    public Point Start, End;
 
-            return obj1.Equals(obj2);
+	public Line(Point start, Point end)
+	{
+		this.Start = start;
+		this.End = end;
+	}
+}
 
-            // Alternative:
-            // return ReferenceEquals(obj1, obj2);
-        }
+public class VectorObject : Collection<Line>
+{
+
+}
+
+public class VectorRectangle : VectorObject
+{
+	public VectorRectangle(int x, int y, int width, int height)
+	{
+        Add(new Line(new Point(x, y), new Point(x + width, y)));
+        Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+        Add(new Line(new Point(x, y), new Point(x, y + height)));
+        Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+    }
+}
+
+
+// ? Assumes lines can only be vertical or horizontals 
+// (for a rectangle, no diagonals)
+public class LineToPointAdapter : Collection<Point>
+{
+	/// <summary>
+	/// Counts invocations
+	/// </summary>
+	private static int count = 0;
+
+	public LineToPointAdapter(Line line)
+	{
+        Console.WriteLine($"\n{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}] (no caching)");
+
+		int left = Math.Min(line.Start.X, line.End.X);
+		int right = Math.Max(line.Start.X, line.End.X);
+		int bottom = Math.Min(line.Start.Y, line.End.Y);
+		int top = Math.Max(line.Start.Y, line.End.Y);
+
+        // dx and dy denote change in x and y coordinates between the start and end points of a line.
+		// dx = 0 implies the line is vertical, dy = 0 implies it is horizontal.
+
+        int dx = right - left;
+		int dy = top - bottom;
+
+		if (dx == 0)
+		{
+			for (int y = bottom; y <= top; y++)
+			{
+				Add(new Point(left, y));
+
+			}
+		} else if (dy == 0)
+		{
+			for (int x = left; x <= right; x++)
+			{
+				Add(new Point(x, top));
+			}
+		}
+    }
+}
+
+public class Demo
+{
+	private static readonly List<VectorObject> vectorObjects = new List<VectorObject>
+	{
+		new VectorRectangle(1, 1, 10, 10),
+		new VectorRectangle(3, 3, 6, 6)
+	};
+
+	// the interface we have
+	public static void DrawPoint(Point p)
+	{
+		Console.Write(".");
+	}
+
+	public static void Main(string[] args)
+	{
+		//DrawPoint(new Point(1, 1));
+		Draw();
+	}
+
+    private static void Draw()
+    {
+		foreach (var vectorObj in vectorObjects)
+		{
+			foreach (var line in vectorObj)
+			{
+				var adapter = new LineToPointAdapter(line);
+				adapter.ForEach(DrawPoint); // MoreLinq
+
+			}
+		}
     }
 }
