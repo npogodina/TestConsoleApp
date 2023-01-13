5123aac3bfa045f435001331879fbd6c57bfbe21
Nataliya Pogodina <n.pogodina@gmail.com>
2023-01-13 08:22:12 -0800

    Composite pattern and IEnumerable

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,62 +1,60 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
 
 namespace Composite;
 
-// Allows to treat both single objects and composite objects (grouping of objects) uniformly
-// For example, Foo and Collection<Foo> have common APIs
-
-public class GraphicObject
+public static class ExtensionMethods
 {
-    public virtual string Name { get; set; } = "Group";
-    public string Color;
-
-
-    private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
-    public List<GraphicObject> Children => children.Value;
-
-    public override string ToString()
+    public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
     {
-        var sb = new StringBuilder();
-        Print(sb, 0);
-        return sb.ToString();
-    }
+        if (ReferenceEquals(self, other)) return;
 
-    private void Print(StringBuilder sb, int depth)
-    {
-        sb.Append(new string('>', depth))
-          .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ") // continue here
-          .AppendLine(Name);
-
-        foreach (var child in Children)
+        foreach (var from in self)
         {
-            child.Print(sb, depth + 1);
+            foreach (var to in other)
+            {
+                from.Out.Add(to);
+                to.In.Add(from);
+            }
         }
     }
 }
 
-public class Circle : GraphicObject
+// Scalar values are treated as composite values with singular component
+// (IEnumerable with 1 item)
+public class Neuron : IEnumerable<Neuron>
 {
-    public override string Name => "Circle";
+    public float Value;
+    public List<Neuron> In = new List<Neuron>();
+    public List<Neuron> Out = new List<Neuron>();
+
+    public IEnumerator<Neuron> GetEnumerator()
+    {
+        yield return this;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        yield return this;
+    }
 }
 
-public class Square : GraphicObject
+public class NeuronLayer : Collection<Neuron>
 {
-    public override string Name => "Square";
+
 }
 
 public class Demo
 {
     static void Main(string[] args)
     {
-        var drawing = new GraphicObject { Name = "My Drawing" };
-        drawing.Children.Add(new Square { Color = "Red" });
-        drawing.Children.Add(new Circle { Color = "Yellow" });
+        var neuron1 = new Neuron();
+        var neuron2 = new Neuron();
+        var layer1 = new NeuronLayer();
+        var layer2 = new NeuronLayer();
 
-        var group = new GraphicObject();
-        group.Children.Add(new Circle { Color = "Blue" });
-        group.Children.Add(new Square { Color = "Blue" });
-        drawing.Children.Add(group);
-
-        Console.WriteLine(drawing);
+        neuron1.ConnectTo(neuron2);
+        neuron1.ConnectTo(layer1);
+        layer1.ConnectTo(layer2);
     }
 }
