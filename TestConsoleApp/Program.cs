de0764fa2d5efd378a90e4364f5d6ac6224678e1
Nataliya Pogodina <n.pogodina@gmail.com>
2023-01-13 12:22:26 -0800

    Composite coding exersice

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,153 +1,44 @@
-﻿namespace Composite;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-public enum Color
+namespace Coding.Exercise
 {
-    Red, Blue, Green
-}
-
-public enum Size
-{
-    Small, Medium, Large
-}
-
-public class Product
-{
-    public string Name;
-    public Color Color { get; set; }
-    public Size Size;
-
-    public Product(string name, Color color, Size size)
+    public interface IValueContainer : IEnumerable<int>
     {
-        if (name == null)
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+
+        // Masquarade as a collection
+        public IEnumerator<int> GetEnumerator()
         {
-            throw new ArgumentNullException(paramName: nameof(name));
+            yield return Value;
         }
 
-        Name = name;
-        Color = color;
-        Size = size;
-    }
-}
-
-// Same interface for a single specification or for multiple!
-public abstract class ISpecification<T>
-{
-    public abstract bool IsSatisfied(T p);
-}
-
-public class ColorSpecification : ISpecification<Product>
-{
-    private Color color;
-
-    public ColorSpecification(Color color)
-    {
-        this.color = color;
-    }
-
-    public override bool IsSatisfied(Product p)
-    {
-        return p.Color == color;
-    }
-}
-
-public class SizeSpecification : ISpecification<Product>
-{
-    private Size size;
-
-    public SizeSpecification(Size size)
-    {
-        this.size = size;
-    }
-
-    public override bool IsSatisfied(Product product)
-    {
-        return product.Size == size;
-    }
-}
-
-public abstract class CompositeSpecification<T> : ISpecification<T>
-{
-    // assignment to the field can only occur as part of the declaration or in a constructor in the same class
-    protected readonly ISpecification<T>[] items;
-
-    public CompositeSpecification(params ISpecification<T>[] items)
-    {
-        this.items = items;
-    }
-}
-
-public class AndSpecification<T> : CompositeSpecification<T> // Combinator
-{
-    public AndSpecification(params ISpecification<T>[] items) : base(items)
-    {
-    }
-
-    public override bool IsSatisfied(T t)
-    {
-        return items.All(i => i.IsSatisfied(t));
-    }
-}
-
-public class OrSpecification<T> : CompositeSpecification<T> // Combinator
-{
-    public OrSpecification(params ISpecification<T>[] items) : base(items)
-    {
-    }
-
-    public override bool IsSatisfied(T t)
-    {
-        return items.Any(i => i.IsSatisfied(t));
-    }
-}
-
-public interface IFilter<T>
-{
-    IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specification);
-}
-
-public class BetterFilter : IFilter<Product>
-{
-    public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> specification)
-    {
-        foreach (var item in items)
+        IEnumerator IEnumerable.GetEnumerator()
         {
-            if (specification.IsSatisfied(item))
-            {
-                yield return item;
-            }
+            return GetEnumerator();
         }
     }
-}
 
-public class Demo
-{
-    static void Main(string[] args)
+    public class ManyValues : List<int>, IValueContainer
     {
-        var apple = new Product("Apple", Color.Green, Size.Small);
-        var tree = new Product("Tree", Color.Green, Size.Large);
-        var house = new Product("House", Color.Blue, Size.Large);
 
-        Product[] products = { apple, tree, house };    
-        
-        var betterFilter = new BetterFilter();
+    }
 
-        Console.WriteLine("Green products:");
-        var greenProducts = betterFilter.Filter(products, new ColorSpecification(Color.Green));
-        foreach (var product in greenProducts)
+    public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
         {
-            Console.WriteLine($" - {product.Name} is green");
-        }
-
-        Console.WriteLine("Large Blue products:");
-        var largeBlueProducts = betterFilter.Filter(
-            products,
-            new AndSpecification<Product>(
-                new ColorSpecification(Color.Blue),
-                new SizeSpecification(Size.Large)
-            ));
-        foreach (var product in largeBlueProducts)
-        {
-            Console.WriteLine($" - {product.Name} is large and blue");
+            int result = 0;
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            return result;
         }
     }
 }
