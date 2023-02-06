1605c9fd50fcd1215f097d507e9a350bca6c23df
Nataliya Pogodina <n.pogodina@gmail.com>
2023-02-06 11:55:35 -0800

    Decorator coding exercise

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,82 +1,55 @@
-﻿namespace Decorator;
+﻿using System;
 
-// Facilitates the addition of behaviors to individual objects without inheriing from them
-// Useful when a class is sealed or when we want to inherit from multiple classes
-//// (in C# a class cannot have multiple base classes)
-
-public interface IBird
+namespace Coding.Exercise
 {
-    int Weight { get; set; }
-
-    void Fly();
-}
-
-public class Bird : IBird
-{
-    public void Fly()
+    public class Bird
     {
-        Console.WriteLine($"Flying with weight {Weight}");
-    }
+        public int Age { get; set; }
 
-    public int Weight { get; set; }
-}
-
-public interface ILizard
-{
-    int Weight { get; set; }
-
-    void Crawl();
-}
-
-public class Lizard : ILizard
-{
-    public void Crawl()
-    {
-        Console.WriteLine($"Crawling with weight {Weight}");
-    }
-
-    public int Weight { get; set; }
-}
-
-public class Dragon : IBird, ILizard
-{
-    private Bird _bird = new Bird();
-    private Lizard _lizard = new Lizard();
-    private int weight;
-
-    public void Crawl()
-    {
-        _lizard.Crawl();
-    }
-
-    public void Fly()
-    {
-        _bird.Fly();
-    }
-
-    // Workaround for situations when both classes implement the same property
-    public int Weight
-    {
-        get
+        public string Fly()
         {
-            return weight;
-        }
-        set
-        {
-            weight = value;
-            _bird.Weight = weight;
-            _lizard.Weight = weight;
+            return (Age < 10) ? "flying" : "too old";
         }
     }
-}
 
-public class Program
-{
-    public static void Main(string[] args)
+    public class Lizard
     {
-        var dragon = new Dragon();
-        dragon.Weight = 100;
-        dragon.Fly();
-        dragon.Crawl();
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        private int _age;
+        private Bird _bird = new Bird();
+        private Lizard _lizard = new Lizard();
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+                _bird.Age = value;
+                _lizard.Age = value;
+            }
+        }
+
+        public string Fly()
+        {
+            return _bird.Fly();
+        }
+
+        public string Crawl()
+        {
+            return _lizard.Crawl();
+        }
     }
 }
